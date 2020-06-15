7cc09cefe140486e8db63ad309f774cde47a6cf4
TOSHIKI SHIMOSAWA <toshiki@TOSHIKInoMacBook-Pro.local>
2020-06-15 11:34:33 +0900

    ログイン保持機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate
+    if user && user.authenticate(params[:session][:password])
       log_in user
+      remember user 
       redirect_to root_url
     else
       flash.now[:danger] = 'メールアドレスとパスワードの組み合わせが間違っています'
@@ -14,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in? 
     redirect_to root_url
   end
 end
